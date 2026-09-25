aed10f43dd4196c91a4161e0df7de03df2818a54
Ridham-tech <gitforme67@gmail.com>
2026-09-25 10:54:18 +0530

    final version addition of two matrix

--- matrix.c
+++ matrix.c
@@ -33,5 +33,15 @@ int main(void){
         }
         printf("\n");
     }
+
+    printf("Addition of two matrix is :\n");
+    
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            c[i][j]=b[i][j]+a[i][j];
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
