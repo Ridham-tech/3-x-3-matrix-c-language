efe99b67bc9408040aca31d794f881d13dba0451
Ridham-tech <gitforme67@gmail.com>
2026-09-25 10:50:49 +0530

    second matrix set up

--- matrix.c
+++ matrix.c
@@ -20,4 +20,18 @@ int main(void){
         printf("\n");
     }
 
+    printf("Enter Second Matrix here :");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("Second Matrix is :\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
